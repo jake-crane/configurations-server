b942bdfc63e70120c877d7f9cbc277293db55327
Jake Crane <jake@jakecrane.com>
2017-08-20 03:51:24 -0500

    Better request validation.

--- src/server.js
+++ src/server.js
@@ -24,26 +24,44 @@ router.get('/', (req, res) => {
     res.send(resp);
 });
 
+function validateConfiguration(config) {
+    var validKeys = ['id', 'key', 'name', 'value', 'description', 'type'];
+    var keys = Object.keys(config);
+    for (var key of keys) {
+        if (!validKeys.includes(key)) {
+            var errorMessage = `Invalid property "${key}" in configuration`;
+            console.error(errorMessage);
+            throw {
+                message: errorMessage,
+                status: 400
+            };
+        }
+    }
+}
+
+function getConfigFromReq(req) {
+    var config = req.body;
+    validateConfiguration(config);
+    return config;
+}
+
 router.post('/', (req, res) => {
-    var newConfiguration = req.body;
-    /*if (Object.keys(newConfiguration).length !== 5) {
-        res.status(400).send('Invalid property in data');
-        return;
-    }*/
+    var newConfiguration = getConfigFromReq(req);
     newConfiguration.id = Math.floor(Math.random() * 1000000) + 1;
     data.configuration.push(newConfiguration);
     res.status(200).send(newConfiguration);
 });
 
 router.put('/:id', (req, res) => {
+    var config = getConfigFromReq(req);
     const index = data.configuration.findIndex(
         configuration => configuration.id == req.params.id
     );
-    if (index === -1 || req.params.id != req.body.id)
+    if (index === -1 || req.params.id != config.id) {
         res.status(400).send('Invalid id or ids do not match');
-    else if (Object.keys(req.body).length !== 6)
-        res.status(400).send('Invalid property in data');
-    data.configuration[index] = req.body;
+        return;
+    }
+    data.configuration[index] = config;
     res.status(204).send();
 });
 
@@ -59,5 +77,8 @@ router.delete('/:id', (req, res) => {
 
 app.use(bodyParser.json());
 app.use('/configurations', router);
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500).send(err.message);
+});
 
 export default app

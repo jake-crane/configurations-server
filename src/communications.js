60a1f2766add50afd599cd024e48eb1e2dab31b5
Jake Crane <jake@jakecrane.com>
2017-11-04 19:40:06 -0500

    Add some basic communications responses

--- src/communications.js
+++ src/communications.js
@@ -1,21 +1,49 @@
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const xmlResponses = {};
 
-function setupRoute(name) {
+
+function setupGetRoute(name) {
     fs.readFile(`./communications/${name}.xml`, 'utf8', (err, data) => {
         router.get(`/${name}`, (req, res) => {
             res.set('Content-Type', 'text/xml');
-            res.send(data);
+            xmlResponses[name] = data;
+            res.send(xmlResponses[name]);
         });
     });
 }
 
-setupRoute('aftsettings');
-setupRoute('clients');
-setupRoute('contentmanagement');
-setupRoute('deliverychannels');
-setupRoute('ftpsettings');
-setupRoute('systemsecurity');
+setupGetRoute('aftsettings');
+setupGetRoute('clients');
+setupGetRoute('contentmanagement');
+setupGetRoute('deliverychannels');
+setupGetRoute('ftpsettings');
+setupGetRoute('systemsecurity');
+
+router.put('/aftsettings', (req, res) => {
+    res.status(200).send(xmlResponses['aftsettings']);
+});
+
+router.put('/clients/:id', (req, res) => {
+    res.status(200).send(xmlResponses['clients']);
+});
+
+router.put('/contentmanagement', (req, res) => {
+    res.status(200).send(xmlResponses['contentmanagement']);
+});
+
+router.put('/deliverychannels', (req, res) => {
+    res.status(200).send(xmlResponses['deliverychannels']);
+});
+
+router.post('/ftpsettings', (req, res) => {
+    res.status(200).send(xmlResponses['ftpsettings']);
+});
+
+router.put('/systemsecurity', (req, res) => {
+    res.status(200).send(xmlResponses['systemsecurity']);
+});
+
 
 module.exports = router;
